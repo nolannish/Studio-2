1cb1a796687d62a0e5235411070542227f203c97
nolannish <nolannish@gmail.com>
2025-02-11 16:31:30 -0800

    Player controller allows for side to side movement of player model now

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     void Update()
     {
@@ -10,5 +11,17 @@ public class InputManager : MonoBehaviour
         {
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A)){
+            input += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+            input += Vector2.right;
+        }
+
+        OnMove?.Invoke(input);
     }
 }
